eae7fba76ec173ad3a34655341ff4ce5a7eabfdf
derleymad <70535956+derleymad@users.noreply.github.com>
2022-04-06 12:31:16 -0300

    Removendo bugs (mesma path)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import os
 
 links = []
+destination = 'musicas'
 
 def clearConsole():
     command = 'clear'
@@ -19,11 +20,25 @@ try:
     for link in links:
         yt = YouTube(link)
         tit = str(yt.title)
-
+        
         print('Baixando ' + tit +'...')
 
         video = yt.streams.filter(only_audio=True).first()
-        musica = video.download("musicas", filename=f'{tit}.mp3')
+        out_file = video.download(output_path=destination)
+
+        base, ext = os.path.splitext(out_file)
+        
+        new_file = base + '.mp3'
+        old_file = base + '.mp4'
+
+        try:
+            os.rename(out_file, new_file)
+        except:
+            if os.path.exists(new_file):
+                print("Oxe, tu já baixou esse ai...")
+                os.remove(old_file)
+                time.sleep(2)
+            
     clearConsole() 
     print("\nBaixou tudo com Sucesso\n")
 
@@ -31,4 +46,4 @@ try:
     time.sleep(2)
 
 except:
-    print("\nAlguma coisa deu errado")
+    print("\nOxe, alguma coisa deu errado")
